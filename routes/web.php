b69873b380b115cc000cd02e9a62f3587d3d8606
tomo0425 <onishi.tomoki@g.wakaya-u.jp>
2022-05-30 10:08:14 +0900

    2章まで

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,13 @@ Route::get('/', function () {
 
 Route::get('/sample', [\App\Http\Controllers\Sample\IndexController::class,'show']);
 
-Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,'showId']);
+Route::get('/sample/{id}', [\App\Http\Controllers\Sample\IndexController::class,'showid']);
+
+Route::get('/tweet', \App\Http\Controllers\Tweet\IndexController::class)
+->name('tweet.index');
+Route::post('/tweet/create', \App\Http\Controllers\Tweet\CreateController::class)
+->name('tweet.create');
+
+Route::get('/tweet/update/{tweetId}',\App\Http\Controllers\Tweet\Update\IndexController::class)->name('tweet.update.index')->where('tweetId', '[0-9]+');
+Route::put('/tweet/updae/{tweetId}', \App\Http\Controllers\Tweet\Update\PutController::class)->name('tweet.update.put')->where('tweetId', '[0-9]+');
+Route::delete('/tweet/delete/{tweetId}', \App\Http\Controllers\Tweet\DeleteController::class)->name('tweet.delete');
